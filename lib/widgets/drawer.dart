e43844ca7c319bd5e5fa519d5b9b80d9db528ea6
mimran25 <mimran@codeteck.com>
2022-10-05 08:02:30 +0500

    Changing done in Header and Grid

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
 
@@ -15,9 +14,8 @@ class MyDrawer extends StatelessWidget {
             DrawerHeader(
               padding: EdgeInsets.zero,
               child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 200, 44, 32)
-                ),
+                decoration:
+                    BoxDecoration(color: Color.fromARGB(255, 200, 44, 32)),
                 margin: EdgeInsets.zero,
                 accountName: Text("Muhammad Imran"),
                 accountEmail: Text("imrange736@gmail.com"),
@@ -26,65 +24,107 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-             ExpansionTile(
-              leading: Icon(
-                CupertinoIcons.home,
-                color: Colors.white,
-              ),
-          title: Text('Heading',
-          textScaleFactor: 1.2,
-          style: TextStyle(
-            color: Colors.white,
-          ),
-           
-          ),
-          children: <Widget>[
-            Center(
-              child: Padding(
-                padding: new EdgeInsets.only(
-                  left: 70,
-                ),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Text('subHeading1',
-                      textScaleFactor: 1.1,
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text('subHeading1',
-                      textScaleFactor: 1.1,
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                      ),
-                    ],
+            ExpansionTile(
+              title: Text("Reception",
+              style: TextStyle(color: Colors.white,fontSize: 18),),
+              leading: Icon(Icons.person,
+              color: Colors.white,
+              ), //add icon
+              childrenPadding: EdgeInsets.only(left: 60),
+              children: [
+                ListTile(
+                  title: Text("Registration",
+                  style: TextStyle(color: Colors.white,fontSize: 16),
                   ),
+                  
+                  onTap: () {
+                    //action on press
+                  },
                 ),
-              ),
+
+                ListTile(
+                  title: Text("Invoice",
+                  style: TextStyle(color: Colors.white,fontSize: 16),),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: Text("Cancel Request",
+                  style: TextStyle(color: Colors.white,fontSize: 16),),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: Text("Credit Note",
+                  style: TextStyle(color: Colors.white,fontSize: 16),),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: Text("Republish Invoice",
+                  style: TextStyle(color: Colors.white,fontSize: 16),),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+                ListTile(
+                  title: Text("Cash Detail",
+                  style: TextStyle(color: Colors.white,fontSize: 16),),
+                  onTap: () {
+                    //action on press
+                  },
+                ),
+
+                //more child menu
+              ],
             ),
-          ],
-        ),
             ListTile(
               leading: Icon(
-                CupertinoIcons.home,
+                Icons.access_alarm,
                 color: Colors.white,
               ),
               title: Text(
-                "Home",
+                "In Patients",
                 textScaleFactor: 1.2,
-                style: TextStyle(
-                  color: Colors.white
-                ),
+                style: TextStyle(color: Colors.white),
               ),
-            )
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.access_alarm,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Out Patients",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.access_alarm,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Trauma Center",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.access_alarm,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Rediology",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           ],
         ),
       ),
